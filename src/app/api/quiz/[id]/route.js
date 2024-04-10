d2734ff8038b510dc641c54d24af25c7b4e9d5e2
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-10 10:16:21 -0300

    otro

--- src/app/api/quiz/[id]/route.js
+++ src/app/api/quiz/[id]/route.js
@@ -9,5 +9,3 @@ export async function DELETE(res, { params }) {
     } catch (error) {
     }
 }
-
-
--- src/libs/mongoose.js
+++ src/libs/mongoose.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
+let mongoClient; 
+
 export async function connectDB(){
-    await mongoose.connect(process.env.MONGODB_URI);
+    try{
+        if(mongoClient){
+            return { mongoClient };
+        }
+        mongoClient = await mongoose.connect(process.env.MONGODB_URI);
+        console.log("conectado...");
+        return { mongoClient };
+    }catch(e){
+        console.log(e);
+    }
 }
